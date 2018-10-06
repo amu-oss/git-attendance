7bc9bc27fea1ee341d909da1eade5b48f24d7c92
prateekj117 <prateekj1171998@gmail.com>
2018-10-06 12:36:45 +0530

    Add user Prateek

---
+++ students/prateek.js
@@ -0,0 +1,11 @@
+export default {
+    "name": "Prateek Jain",
+    "class": "2nd Year",
+    "description": "Hey everyone !!",
+    "color": "purple",
+    "avatar": "https://avatars0.githubusercontent.com/u/3874064?s=460&v=4",
+    "hobbies": [
+        "Web development",
+        "Competitive Programming"
+    ]
+}
