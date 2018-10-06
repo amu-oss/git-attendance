fa28a057f911341ac63586392de5f883bfb4862b
Mohd Omama <mohd.omama@gmail.com>
2018-10-06 14:08:00 +0530

    cat

--- students/index.js
+++ students/index.js
@@ -3,11 +3,13 @@ import vishal from './vishal.js'
 import roshan from './roshan.js'
 import husain from './husain.js'
 import prateek from './prateek.js'
+import mustajab from './mustajab.js'
 
 export default [
     areeb,
     prateek,
     husain,
     roshan,
-    vishal
+    vishal,
+    mustajab
 ]
---
+++ students/mustajab.js
@@ -0,0 +1,12 @@
+export default {
+    "name": "MUSTAJAB IKRAM",
+    "class": "BTECH-FIRST YEAR, COB",
+    "description": "I am MUSTAJAB IKRAM. I love to program things in Python, JS, and PHP. I am a fan of Call of Duty and Counter-Strike",
+    "color": "WHITE",
+    "avatar": "http://images4.fanpop.com/image/photos/20500000/Cute-cat-random-20510717-540-720.jpg",
+    "hobbies": [
+        "Web",
+        "Algorithms",
+        "HOCKEY"
+    ]
+}
