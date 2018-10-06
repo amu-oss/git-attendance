19609af93fba953b49d4283857e978022fef966c
prateekj117 <prateekj1171998@gmail.com>
2018-10-06 12:41:26 +0530

    Add profile pic URL

--- students/prateek.js
+++ students/prateek.js
@@ -3,7 +3,7 @@ export default {
     "class": "2nd Year",
     "description": "Hey everyone !!",
     "color": "purple",
-    "avatar": "https://avatars0.githubusercontent.com/u/3874064?s=460&v=4",
+    "avatar": "https://avatars0.githubusercontent.com/u/31065687?s=400&u=30446145d25d1c6179d0058324d795b6279aa81c&v=4",
     "hobbies": [
         "Web development",
         "Competitive Programming"
